f67dc1ab3e9bf57d42ac4029ebb6ffccad3ab225
nahom zenebe <nahomzenebe123@gmail.com>
2025-04-23 12:19:59 +0300

    category state creation

---
+++ book_store_app/lib/Features/Category/presentation/Categorystate.dart
@@ -0,0 +1,36 @@
+
+import 'package:book_store_app/Features/Category/domain/Categoryentities.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class Categorystate extends Equatable {
+  const Categorystate();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CategoryInitial extends Categorystate {
+  const CategoryInitial();
+}
+
+class CategoryLoading extends Categorystate {
+  const CategoryLoading();
+}
+
+class CategoryLoaded extends Categorystate {
+  final List<Categoryentities> Category;
+
+  const CategoryLoaded(this.Category);
+
+  @override
+  List<Object> get props => [Category];
+}
+
+class CategoryError extends Categorystate {
+  final String message;
+
+  const CategoryError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
