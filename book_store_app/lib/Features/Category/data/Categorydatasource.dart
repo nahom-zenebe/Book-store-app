9bd7b60f94c89558b5348d1695b8b4fe3260c989
nahom zenebe <nahomzenebe123@gmail.com>
2025-04-23 11:58:26 +0300

    creating create and getall data source for category

--- book_store_app/lib/Features/Category/data/Categorydatasource.dart
+++ book_store_app/lib/Features/Category/data/Categorydatasource.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:book_store_app/Features/Category/data/Categorymodel.dart';
 import 'package:http/http.dart' as http;
 
 const String baseUrl = "http://localhost:5000/api";
@@ -6,10 +7,36 @@ const String baseUrl = "http://localhost:5000/api";
 class Categorydatasource {
   Future<void> createCategory(String name, String description) async {
     try {
-      final response = await http.post(Uri.parse("$baseUrl/createcatgory"));
+      final response = await http.post(Uri.parse("$baseUrl/createcatgory"),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode({"name": name, "description": description}));
       if (response.statusCode == 201) {
         print("Created successfullly");
+      } else {
+        throw Exception("Error in creating product");
       }
-    } catch (error) {}
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<List<Categorymodel>> getallCategory() async {
+    try {
+      final response = await http.get(Uri.parse("$baseUrl/getallcatgory"));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        return data
+            .map((category) => Categorymodel.fromjson(category))
+            .toList();
+      } else {
+        throw Exception("Error in getting categories");
+      }
+    } catch (error) {
+      print(error);
+      return [];
+    }
   }
 }
